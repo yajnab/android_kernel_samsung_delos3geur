aa6711639c239847e4b692ef83f24385eb7274f2
Yajnavalkya Bandyopadhyay <yajnab@gmail.com>
2014-07-13 21:28:52 +0530

    Update to 3.4.9

--- arch/arm/kernel/process.c
+++ arch/arm/kernel/process.c
@@ -329,6 +329,7 @@ void machine_shutdown(void)
 void machine_halt(void)
 {
 	machine_shutdown();
+	local_irq_disable();
 	while (1);
 }
 
@@ -354,6 +355,7 @@ void machine_restart(char *cmd)
 
 	/* Whoops - the platform was unable to reboot. Tell the user! */
 	printk("Reboot failed -- System halted\n");
+	local_irq_disable();
 	while (1);
 }
 
